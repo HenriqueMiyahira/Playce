a09aed482bc9e0fcb3ff6a5759f8462915317f50
Henrique Miyahira <128231899+HenriqueMiyahira@users.noreply.github.com>
2026-06-03 11:10:45 -0300

    Delete Aplicativo De Gerenciamento De Quadras directory

--- Aplicativo De Gerenciamento De Quadras/home/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-# Create your models here.
-#bertoncello - o django já tem o URM maius poderoso do mercado, então eu voiu criar uma tabela simples aqui e que já está pronta
-class Usuarios(models.Model):
-    nome = models.CharField(max_length=50) #criando a coluna 'nome' models.charfield é o tipo, o modelo, o final é quantos caracteres ao máximo
-    email = models.EmailField(max_length=50) #criando a coluna emai dessa vendo só definindo o modelo sem máximo de caracteres 
