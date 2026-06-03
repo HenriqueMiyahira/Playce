e0cb640e3248cacda697047f7559b0d98df9cbf9
Iaanzen <iaaanzen@gmail.com>
2026-06-03 14:42:29 -0300

    Estrutura inicial do projeto Playce

---
+++ home/views.py
@@ -0,0 +1,13 @@
+#aqui é onde fica a logica do usuário, o que ele quer e o que ele clica
+from django.shortcuts import render
+
+from .models import Usuarios
+
+# Create your views here.
+def home(request):
+    #user = Usuarios(nome='Pedro', email='pedrobardo@gmail.com')
+    #user.save()
+    #usuario = Usuarios.objects.get(id=11)
+    #usuario.delete()
+
+    return render(request, 'index.html')
